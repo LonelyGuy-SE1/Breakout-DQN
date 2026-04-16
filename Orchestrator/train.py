b39a119c96da2e739c548a0af585ee83a668b2ff
LonelyGuy-SE1 <brawlers.assemble@gmail.com>
2026-04-16 19:28:04 +0530

    train script + eval script

---
+++ Orchestrator/train.py
@@ -0,0 +1,72 @@
+import torch
+import numpy as np
+import gymnasium as gym
+import ale_py
+
+from Preprocessing.prep_pipeline import AtariPipeline
+from Memory.buffer import LazyReplayBuffer
+from Agent.dqn_agent import DQNAgent
+
+#modify as desired
+MAX_FRAMES=50000
+BATCH_SIZE=32
+BUFFER_CAPACITY=25000
+SYNC_TARGET_FRAMES=1000 # target network clones policy network
+
+EPSILON_START=1,0
+EPSILON_END=0.1
+EPSILON_DECAY=25000 #frame at which epsilon hits the minimum
+
+def train():
+    print("Starting Orchestratior!")
+    gym.register_envs(ale_py)
+    env=gym.make("ALE/Breakout-v5", render_mode="human")
+    pipeline=AtariPipeline(stack_size=4,screen_size=84)
+    buffer=LazyReplayBuffer(capacity=BUFFER_CAPACITY)
+    agent=DQNAgent(state_shape=(4,84.84), num_actions=4)
+    
+    frame_idx=0
+    episode_reward=0
+    
+    raw_obs, _ = env.reset()
+    state=pipeline.reset(raw_obs)
+    
+    print("Starting training!")
+    
+    while frame_idx<MAX_FRAMES:
+        
+        epsilon=max(EPSILON_END,EPSILON_START-frame_idx*(EPSILON_START-EPSILON_END)/EPSILON_DECAY)
+        action=agent.act(state,epsilon)
+        
+        raw_obs, reward, terminated, truncated, _ =env.step(action)
+        done = terminated or truncated
+        next_state=pipeline.step(raw_obs)
+        
+        buffer.add(next_state[-1],action, reward, done)
+        
+        state=next_state
+        episode_reward+=reward
+        frame_idx+=1
+        
+        if frame_idx>BATCH_SIZE:
+            batch=buffer.sample(BATCH_SIZE)
+            loss=agent.learn(batch)
+            
+        if frame_idx % SYNC_TARGET_FRAMES==0:
+            agent.update_target_network()
+            print(f"Frame {frame_idx} | Syncing Target Network | Epsilon: {epsilon:.2f} | Loss: {loss:.4f}")
+            
+        if done: 
+            print(f"Game Over | Frames Survived: {frame_idx} | Score: {episode_reward}")
+            raw_obs, _ = env.reset()
+            state = pipeline.reset(raw_obs)
+            episode_reward = 0
+            
+        print("Training Complete!")
+        env.close
+        
+    if __name__=="__main__":
+        train()
+                
+
+
---
+++ Tests/eval.py
@@ -0,0 +1,38 @@
+import gymnasium as gym
+import torch
+import ale_py
+from Preprocessing.prep_pipeline import AtariPipeline
+from Agent.network import DQN
+
+def deploy_agent():
+    gym.register_envs(ale_py)
+    env = gym.make("ALE/Breakout-v5", render_mode="human")
+    pipeline = AtariPipeline(stack_size=4, screen_size=84)
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    brain = DQN(input_channels=4, num_actions=4).to(device)
+    
+    print("Loading weights...")
+    brain.load_state_dict(torch.load("dqn_brain.pth", map_location=device))
+    brain.eval() 
+    
+    raw_obs, _ = env.reset()
+    state = pipeline.reset(raw_obs)
+    done = False
+    
+    print("Agent Deployed!")
+    while not done:
+        state_tensor = torch.FloatTensor(state).unsqueeze(0).to(device)
+        
+        with torch.no_grad():
+            q_values = brain(state_tensor)
+            action = q_values.argmax(dim=1).item()
+            
+        raw_obs, reward, terminated, truncated, _ = env.step(action)
+        done = terminated or truncated
+        state = pipeline.step(raw_obs)
+
+    env.close()
+
+if __name__ == "__main__":
+    deploy_agent()
